7ec834f71e1f145cf86b3787153b5b9094f8426c
Rico Sta. Cruz <rico@ambiescent.com>
2011-05-13 06:28:07 +0800

    ~ Modularize

---
+++ lib/notedown/document.rb
@@ -0,0 +1,17 @@
+module Notedown
+class Document
+  attr_reader :source
+
+  def initialize(source)
+    @source = source
+  end
+
+  def root
+    @root ||= Node.new(nil, '', source.split("\n"))
+  end
+
+  def to_html
+    root.to_html
+  end
+end
+end
